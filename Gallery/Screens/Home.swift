286f1a1223f84a67ad47bc841c1da6faa22e7ef4
xx-wu <joyheihei@me.com>
2020-11-07 15:02:16 +0700

    request

--- Gallery/Screens/Home.swift
+++ Gallery/Screens/Home.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Home: View {
 	let imageWidth = UIScreen.main.bounds.width * 0.95
+	let req = List()
 	
 	var body: some View {
 		NavigationView {
@@ -30,6 +31,7 @@ struct Home: View {
 					}
 					HStack{
 						Text("Life is a characteristic that distinguishes physical entities that have biological processes, such as signaling and self-sustaining processes...")
+							.lineLimit(2)
 							.foregroundColor(Color.secondary)
 							.padding(.leading)
 						Spacer()
@@ -69,6 +71,15 @@ struct Home: View {
 			.navigationBarTitle("Home")
 		}
 	}
+}
+
+func List() -> String {
+	let req = Request(URL: "http://www.google.com", Method: "GET", Params: "")
+	req.DO()
+	print("request...")
+	print(req)
+	
+	return "..."
 }
 
 struct Home_Previews: PreviewProvider {
--- Gallery/Utils/Request.swift
+++ Gallery/Utils/Request.swift
@@ -8,10 +8,36 @@
 import Foundation
 
 class Request {
+	let Base:String = "http://localhost:81"
 	let Path:String
+	let Method:String
+	let Params:String?
 	
-	init(URL:String, Mothed:String, Data:Any) {
+	init(URL:String, Method:String, Params:String) {
 		self.Path = URL
+		self.Method = Method
+		self.Params = Params
+	}
+
+	func DO(completion:@escaping (Data?) -> Void) {
+		
+		guard let path = URL(string: self.Path) else { return }
+		
+		var UrlRequest = URLRequest(url: path)
+		UrlRequest.httpMethod = self.Method
+		
+		if self.Params != "" {
+			UrlRequest.httpBody = Params?.data(using: String.Encoding.utf8)
+		}
+
+		let task = URLSession.shared.dataTask(with: UrlRequest) { (data, response, error) in
+			guard let data = data else { return }
+			print(String(data: data, encoding: .utf8)!)
+			
+			completion(data)
+		}
+
+		task.resume()
 	}
 	
 }
